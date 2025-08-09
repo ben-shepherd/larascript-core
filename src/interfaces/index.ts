7fa9cb9153c4b9a7ff67e5f0884d0d904e6c1d04
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-08-09 18:36:19 +0100

    refactor: remove EnvService export from index.ts

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,7 +1,6 @@
 export * from "./ClassConstructor.t";
 export * from "./Dependency.t";
 export * from "./EnvironmentType.t";
-export * from "./EnvService.t";
 export * from "./HasConfigConcern.t";
 export * from "./Provider.t";
 export * from "./Singleton.t";
