bf883ab5102551e9bdd56f63b32c2581793cb1cd
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-08-09 20:46:13 +0100

    refactor: remove unused exports from index files to streamline codebase

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,6 @@
-export * from "./ClassConstructor.t";
 export * from "./Dependency.t";
 export * from "./EnvironmentType.t";
 export * from "./HasConfigConcern.t";
 export * from "./Provider.t";
 export * from "./Singleton.t";
+
