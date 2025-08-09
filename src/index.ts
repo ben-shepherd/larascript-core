9c31a84618421cf6081064887767eb75dbe98c1d
BENJAMIN\bensh <ben.shepherd@gmx.com>
2025-08-09 18:24:24 +0100

    feat: add EnvService for managing environment variables and update package version

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export * from "./consts";
 export * from "./exceptions";
 export * from "./interfaces";
 export * from "./kernel";
+export * from './services';
 export * from "./utils";
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,6 +1,8 @@
 export * from "./ClassConstructor.t";
 export * from "./Dependency.t";
 export * from "./EnvironmentType.t";
+export * from "./EnvService.t";
 export * from "./HasConfigConcern.t";
 export * from "./Provider.t";
 export * from "./Singleton.t";
+
